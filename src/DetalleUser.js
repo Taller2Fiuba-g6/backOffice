d2ac923aa8eefa66d7feee3587233fabf133f51c
Hugo <hhuzan@fi.uba.ar>
2023-11-20 10:43:36 -0300

    update DetalleUser

--- src/DetalleUser.js
+++ src/DetalleUser.js
@@ -2,11 +2,13 @@ const DetalleUser = ({ selectedUser }) => {
     if (typeof selectedUser !== "undefined") {
         return (
             <section className="detalle_user">
+                <h2>Cover Photo:</h2>
                 <picture className="detalle_user_picture">
                     <img src={selectedUser.coverPhoto} alt="" />
                 </picture>
+                <h2>Profile Photo:</h2>
                 <picture className="detalle_user_picture">
-                    <img src={selectedUser.profilePhoto} alt="" />
+                    <img className="detalle_user_img" src={selectedUser.profilePhoto} alt="" />
                 </picture>
             </section>
         );
